cc738d02b5f723a67c4e88e34f9655d5fbc770c6
Immah007 <112688332+Immah007@users.noreply.github.com>
2026-02-05 22:52:03 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const productRoutes = require('./routes/product-routes');
+const crudRoutes = require('./routes/crud-routes');
+const checkoutRoutes = require('./routes/checkout-routes');
+const PORT = 4000;
+ 
+
+app.use(express.static(path.join(__dirname, 'public')));
+// For JSON:
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Endpoint to get products
+app.use('/fetch', productRoutes);
+app.use('/crud', crudRoutes);
+app.use('/checkout', checkoutRoutes);
+
+
+app.get('/awake', (req, res) => {
+  res.send('System Awoken');
+});
+
+// Serve index.html for the main site
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'new-ux.html'));
+});
+
+app.get('/old-page', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'old-ux.html'));
+});
+
+
+// Serve admin.html for the admin panel
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+});
+
+// Serve admin.html for the admin panel
+app.get('/admin-b', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'admin-b.html'));
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
